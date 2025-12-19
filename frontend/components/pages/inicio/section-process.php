184dca41913a6e1715e0e60e78061dddb1733849
Denil Dinis <denildinis1@gmail.com>
2025-12-19 09:48:44 -0300

    Add files via upload

---
+++ frontend/components/pages/inicio/section-process.php
@@ -0,0 +1,47 @@
+<section class="process" id="process">
+  <div class="title">
+    <h2>Como Funciona?</h2>
+  </div>
+
+  <div class="steps">
+    <div class="step">
+      <span class="step-number">1</span>
+      <div class="details">
+        <div class="text">
+          <h4>Preencha o formulário</h4>
+          <p>Informe seu nome, telefone, agência e o serviço desejado.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="step">
+      <span class="step-number">2</span>
+      <div class="details">
+        <div class="text">
+          <h4>Receba sua ficha</h4>
+          <p>Após o envio, a plataforma gera uma ficha com seus dados e senha.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="step">
+      <span>3</span>
+      <div class="details">
+        <div class="text">
+          <h4>Dirija-se à agência</h4>
+          <p>Seu atendimento será realizado conforme a ordem das senhas.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="step">
+      <span>4</span>
+      <div class="details">
+        <div class="text">
+          <h4>Aguarde o atendimento</h4>
+          <p>Seu atendimento será realizado conforme a ordem das senhas.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
