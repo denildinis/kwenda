14c8ea429dd06b45dba5d23c2a3dce340301a596
Denil Dinis <denildinis1@gmail.com>
2026-01-05 05:01:47 +0100

    Delete frontend directory

--- frontend/components/global/nav/nav.php
+++
@@ -1,9 +0,0 @@
-<header>
-  <a href="" class="logo"><img src="/kwenda/frontend/assets/images/logo.svg" alt="logo" /></a>
-  <nav class="options">
-    <a href="/kwenda/inicio">Início</a>
-    <a href="/kwenda/inicio/#benefits">Benefícios</a>
-    <a href="/kwenda/inicio/#process">Como funciona</a>
-  </nav>
-  <a href="/kwenda/frontend/pages/agendar/agendar.php" class="schedule">Fazer agendamento</a>
-</header>
