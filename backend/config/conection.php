ed7911ed80d07652800fae23bcce355781cc05e6
Denil Dinis <denildinis1@gmail.com>
2026-01-09 09:28:45 +0100

    Delete backend directory

--- backend/config/conection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=kwenda", "root", "");
-if (!$pdo) {
-  die();
-}
--- backend/routes/registerBank.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Content-Type: application/json");
-require_once __DIR__ . '/../controller/bankController.php';
-echo registerBank($_POST, $_FILES);
