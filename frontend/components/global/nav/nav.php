e8911c0b96078da5ed7b846ba5fff0c15c645e9b
Denil Dinis <denildinis1@gmail.com>
2026-01-09 09:28:55 +0100

    Delete frontend directory

--- frontend/components/global/nav/nav.php
+++
@@ -1,9 +0,0 @@
-<header>
-  <a href="#" class="logo"><img src="../frontend/assets/images/logo.svg" alt="logo" /></a>
-  <nav class="options">
-    <a href="#">Início</a>
-    <a href="#benefits">Benefícios</a>
-    <a href="#process">Como funciona</a>
-  </nav>
-  <a href="#schedule" class="schedule">Agendar atendimento</a>
-</header>
