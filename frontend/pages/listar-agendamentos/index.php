f2ae6b5682ae6878a81ee15529d36580d94a56f9
Denil Dinis <denildinis1@gmail.com>
2026-01-18 17:01:24 -0300

    Add files via upload

---
+++ frontend/pages/listar-agendamentos/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . "/../../../backend/functions/sessions/session.php";
+verifySession();
+?>
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="../../assets/favicons/favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="../../assets/css/pages/listar-bancos/index.css" />
+  <title>Bancos | Kwenda</title>
+</head>
+
+<body>
+  <?php include "../../components/global/icons/icons.php"; ?>
+  <?php include "../../components/global/alert/alert.php"; ?>
+  <?php include "../../components/global/top/top.php"; ?>
+  <?php include "../../components/pages/listar-agendamentos/section-list.php"; ?>
+</body>
+<script src="../../assets/js/global/alert.js"></script>
+<script src="../../assets/js/global/header.js"></script>
+<script src="../../assets/js/global/dropdown.js"></script>
+<script src="../../assets/js/pages/listar-agendamentos/list-schedules.js"></script>
+<script src="../../assets/js/pages/atender-agendamento/attend-schedule.js"></script>
+<script src="../../assets/js/pages/cancelar-agendamento/cancel-schedule.js"></script>
+</html>
